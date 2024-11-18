a0da83626dcba121324e1f74ac553a6522e7e66c
Zeroback101 <tmddus7776@naver.com>
2024-11-18 17:53:09 +0900

    Create 3.8_Enumeration.scala

---
+++ 3.8_Enumeration.scala
@@ -0,0 +1,35 @@
+object Enum extends App {
+  enum Color {
+    case Red, Green, Blue
+  }
+  // Access Enum
+  println(Color.Red)
+  // Access with String
+  println(Color.valueOf("Red"))
+  // Access the position
+  println(Color.Red.ordinal) // ordinal: index로 출력
+  // List all the enum 
+  // values: array를 사용
+  Color.values.foreach(color => println(color))
+
+  // enum with constructor
+  enum Planet(val mass: Double, radius: Double) {
+    case Earth extends Planet(5.97e24, 6.378e6)
+    case Mars extends Planet(6.39e23, 3.389e6)
+    // other planets...
+  }
+  println(Planet.Earth.mass)
+
+  // enum can have method
+  enum Shape {
+    case Circle(radius: Double)
+    case Rectangle(width: Double, height: Double)
+
+    def area: Double = this match {
+      case Circle(r) => Math.PI * r * r
+      case Rectangle(w, h) => w * h
+    }
+  }
+  val shape: Shape = Shape.Circle(10)
+  println(shape.area)
+}
