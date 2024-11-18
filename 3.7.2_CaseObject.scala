37141b577cf02b5f26b1b0858177391fe130f940
Zeroback101 <tmddus7776@naver.com>
2024-11-18 17:48:15 +0900

    Create 3.7.2_CaseObject.scala

---
+++ 3.7.2_CaseObject.scala
@@ -0,0 +1,10 @@
+object CaseObject extends App {
+  case object Logger {
+    def log(message: String): Unit = {
+      println(s"Log: $message")
+    }
+  }
+
+  // Using the case object
+  Logger.log("This is a log message")
+}
